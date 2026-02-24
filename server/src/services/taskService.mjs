3270b637741bc5207a5fd0cc16a4e4109d96154c
niamul007 <am3994566@gmail.com>
2026-02-24 21:53:25 +0600

    adding task service sql

--- server/src/services/taskService.mjs
+++ server/src/services/taskService.mjs
@@ -13,7 +13,32 @@ export const createNewTask = async (userId, title) => {
 };
 
 
+export const getUserTasks = async (userId) => {
+    // We want all tasks where the owner is this user
+    const sql = `SELECT * FROM tasks WHERE user_id = $1 ORDER BY created_at DESC`;
+    const result = await pool.query(sql, [userId]);
+    return result.rows; // Note: We return .rows (all tasks), not just .rows[0]
+}
 
+
+export const deleteTask = async (taskId, userId) => {
+  const sql = `DELETE FROM tasks WHERE id = $1 AND user_id = $2 RETURNING *`;
+  const result = await pool.query(sql, [taskId, userId]);
+  return result.rowCount > 0; // Returns true if something was actually deleted
+};
+
+
+
+export const updateTaskStatus = async (taskId, userId, completed) => {
+  const sql = `
+    UPDATE tasks 
+    SET completed = $3 
+    WHERE id = $1 AND user_id = $2 
+    RETURNING *`;
+  
+  const result = await pool.query(sql, [taskId, userId, completed]);
+  return result.rows[0];
+};
 
 
 
