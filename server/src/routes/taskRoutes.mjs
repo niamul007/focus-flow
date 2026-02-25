15e96accf86d3ff53442f2b759bed25f48ae0596
niamul007 <am3994566@gmail.com>
2026-02-25 15:51:35 +0600

    adding title updates routes

--- server/src/routes/taskRoutes.mjs
+++ server/src/routes/taskRoutes.mjs
@@ -10,5 +10,6 @@ router.get('/', taskCtrl.getTasks);      // GET all my tasks
 router.post('/', taskCtrl.createTask);    // POST a new task
 router.patch('/:id', taskCtrl.updateTask); // PATCH (update) a specific task
 router.delete('/:id', taskCtrl.removeTask); // DELETE a specific task
+router.put('/:id', taskCtrl.updateTaskTitle); //To update the title 
 
 export default router;
