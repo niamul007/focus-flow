1ff5a3115c2ea8358f8d1d0807a52e184b905992
niamul007 <am3994566@gmail.com>
2026-02-25 17:35:26 +0600

    adding auth routes and validator

--- server/src/routes/auth.mjs
+++ server/src/routes/auth.mjs
@@ -1,6 +1,8 @@
-import express from 'express';
-import { register, login } from '../controllers/authController.mjs';
-import { protect } from '../middleware/authMiddleware.mjs'; // <--- Add this!
+import express from "express";
+import { register, login } from "../controllers/authController.mjs";
+import { protect } from "../middleware/authMiddleware.mjs"; // <--- Add this!
+import { signupSchema, loginSchema } from "../validations/authValidation.mjs";
+import { validate } from "../validations/validate.mjs";
 
 const router = express.Router();
 
@@ -9,7 +11,7 @@ router.get("/me", protect, (req, res) => {
   res.json({ message: "You are authorized!", user: req.user });
 });
 
-router.post("/signup", register);
-router.post("/login", login);
+router.post("/signup", validate(signupSchema), register);
+router.post("/login", validate(loginSchema), login);
 
 export default router;
