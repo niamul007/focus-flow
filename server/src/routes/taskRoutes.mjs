916cab8eb8670a0dcb4ace2ea6fb0feb2525bcb2
niamul007 <am3994566@gmail.com>
2026-02-25 17:36:19 +0600

    adding validator to the task

--- server/src/routes/taskRoutes.mjs
+++ server/src/routes/taskRoutes.mjs
@@ -1,13 +1,15 @@
 import express from 'express';
 import * as taskCtrl from '../controllers/taskController.mjs';
 import { protect } from '../middleware/authMiddleware.mjs';
+import { validate } from '../validations/validate.mjs';
+import { createTaskSchema } from '../validations/taskValidation.mjs';
 
 const router = express.Router();
 
 router.use(protect); // The Bouncer protects all task routes
 
 router.get('/', taskCtrl.getTasks);      // GET all my tasks
-router.post('/', taskCtrl.createTask);    // POST a new task
+router.post('/',validate(createTaskSchema), taskCtrl.createTask);    // POST a new task
 router.patch('/:id', taskCtrl.updateTask); // PATCH (update) a specific task
 router.delete('/:id', taskCtrl.removeTask); // DELETE a specific task
 router.put('/:id', taskCtrl.updateTaskTitle); //To update the title 
