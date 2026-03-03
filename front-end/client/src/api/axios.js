7ff6308d4eeb020cdac3f6d1a767f29916d1fea3
niamul007 <am3994566@gmail.com>
2026-03-03 21:36:23 +0600

    adding documentation for each lien

--- front-end/client/src/api/axios.js
+++ front-end/client/src/api/axios.js
@@ -1,16 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
+/**
+ * 1. THE BASE STATION (Axios Instance)
+ * Instead of typing 'http://localhost:5000/api' in every file,
+ * we create a 'pre-configured' version of axios called API.
+ */
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Make sure this is 5000!
+  // This is the 'Home Address' of your Backend server.
+  baseURL: "http://localhost:5000/api", 
 });
 
-// This sends your token automatically with every request
+/**
+ * 2. THE REQUEST INTERCEPTOR (The Security Guard)
+ * This function 'intercepts' every single outgoing request 
+ * BEFORE it leaves your computer and hits the internet.
+ */
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  // A. Check the 'LocalStorage' drawer for the Golden Ticket (Token)
+  const token = localStorage.getItem("token");
+
+  // B. If a token exists, 'GLUE' it to the Authorization Header.
+  // We use the 'Bearer' scheme, which is the industry standard.
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
+  // C. Return the 'config' (The Manifest) to let the request continue its journey.
   return config;
 });
 
+/**
+ * 3. EXPORTING THE TOOL
+ * Now, in any other file, you can just do:
+ * API.get('/tasks') instead of the full axios.get(...) URL.
+ */
 export default API;
