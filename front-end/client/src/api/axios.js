d3df81d8dd9272806cb987111679a55338d963ce
niamul007 <am3994566@gmail.com>
2026-03-08 18:39:05 +0600

    adding axios

--- front-end/client/src/api/axios.js
+++ front-end/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://focus-flow-api.onrender.com/api",
+  baseURL: "https://focus-flow-x7gk.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
