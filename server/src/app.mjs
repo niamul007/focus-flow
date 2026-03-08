f551ce19dd9ca4c894a7496c0a57dcb03685ba87
niamul007 <am3994566@gmail.com>
2026-03-08 18:38:44 +0600

    adding cors

--- server/src/app.mjs
+++ server/src/app.mjs
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     "https://focusflow-frontend-seven.vercel.app",
-    "https://focusflow-frontend-seven.vercel.app/",
     "http://localhost:5173",
     "http://localhost:5174",
   ],
